f891f7b1d897ab5b1d883222a90a325187855f7d
ReginaldUG <reg4real26@gmail.com>
2024-04-19 23:01:55 +0100

    Sending LandMark data to the server, landmarks being shown as they are read in real time

--- HandTrackingCode.py
+++ HandTrackingCode.py
@@ -1,6 +1,15 @@
 import cv2
 import mediapipe as mp
 import time
+import socket
+
+HOST = '192.168.43.81'
+PORT = 9090
+
+socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+socket.connect((HOST, PORT))
+
+
 
 cap = cv2.VideoCapture(0)  # zero uses the default webcam (if you have one camera)
 # sets width and height respectively
@@ -30,8 +39,12 @@ while True:
                 #print(id,lm)
                 height, width, channel = img.shape
                 cx, cy = int(lm.x*width), int(lm.y*height)          #multiply the decimal values with the image pixel positions
-                print(id,cx,cy)
+                #print(id,cx,cy)
+                stringvalue = f"{id}: {cx} {cy}"
+                print(stringvalue)
+                socket.send(stringvalue.encode('utf-8'))            #send the landmarks to the server
 
+            print("H E R E")
             mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS)           #draws the points for the landmarks
 
     cTime = time.time()
@@ -42,5 +55,11 @@ while True:
     cv2.putText(img, str(int(fps)),(10,70),cv2.FONT_HERSHEY_SIMPLEX,3,(255,255,255),2,cv2.LINE_AA)
 
     cv2.imshow("Video", img)
-    cv2.waitKey(1)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+socket.close()
+cap.release()
+cv2.destroyAllWindows()
 
